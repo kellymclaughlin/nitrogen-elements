de28dfffc013b055ca6a0d33747b65750b1d62f8
Kelly McLaughlin <kmclaughlin@absolute-performance.com>
2009-06-01 08:41:46 -0600

    Added trigger option to select how menu is triggered

--- nitrogen_extended/element_menu.erl
+++ nitrogen_extended/element_menu.erl
@@ -2,7 +2,7 @@
 -compile(export_all).
 
 -include_lib("lib/nitrogen/include/wf.inc").
--record(menu, {?ELEMENT_BASE(element_menu), text="", body=[]}).
+-record(menu, {?ELEMENT_BASE(element_menu), trigger="click", text="", body=[]}).
 
 % desc: Provides a collapsable menu. The menu options are specified
 % as listitem elements in the body. 
@@ -10,7 +10,12 @@
 reflect() -> record_info(fields, menu).
 
 render(ControlID, Record) -> 
-	Script = wf:f("$('#~s ul').hide();~n$('#~s li a').click(~nfunction() {~n$(this).next().slideToggle('normal');~n}~n);", [ControlID, ControlID]),
+    Script = case Record#menu.trigger of
+        "hover" ->
+            wf:f("$('#~s ul').hide();~n$('#~s li a').hover(~nfunction() {~n$(this).next().slideToggle('normal');~n}~n);", [ControlID, ControlID]);
+        _ ->
+            wf:f("$('#~s ul').hide();~n$('#~s li a').click(~nfunction() {~n$(this).next().slideToggle('normal');~n}~n);", [ControlID, ControlID])
+    end,
 	wf:wire(Script),
     Title = wf:render(Record#menu.text),
 	Content = wf:render(Record#menu.body),
